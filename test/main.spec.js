879b8745e61b73fb3c8a3e01a77cdb30ee0da1b8
Stefan Penner <stefan.penner@gmail.com>
2015-10-28 16:49:21 -0700

    upgrade to broccoli-persistent-filter
    * faster rebuilds
    * faster warm initial builds

--- test/main.spec.js
+++ test/main.spec.js
@@ -4,34 +4,29 @@ var assert = require('assert');
 var TemplateFilter = require('../index');
 
 describe('broccoli-ember-hbs-template-compiler', function() {
-  it('it sets inputTree', function() {
-    var filter = new TemplateFilter('templates');
-    assert(filter.inputTree == 'templates');
-  });
-
   it('it sets options', function() {
     var filter = new TemplateFilter('templates', {module: true});
-    assert(filter.options.module === true);
+    assert(filter.options.module);
   });
 
   it('returns a precompiled global template', function() {
     var filter = new TemplateFilter('templates');
     var template = filter.processString('foo', './templates/foo.hbs');
     var expected = fs.readFileSync(path.resolve(__dirname, 'expected-global.js')).toString();
-    assert(template === expected);
+    assert.equal(template, expected);
   });
 
   it('returns a precompiled module template', function() {
     var filter = new TemplateFilter('templates', {module: true});
     var template = filter.processString('foo', './templates/foo.hbs');
     var expected = fs.readFileSync(path.resolve(__dirname, 'expected-module.js')).toString();
-    assert(template === expected);
+    assert.equal(template, expected);
   });
 
   it('returns a precompiled commonjs template', function() {
     var filter = new TemplateFilter('templates', {commonjs: true});
     var template = filter.processString('foo', './templates/foo.hbs');
     var expected = fs.readFileSync(path.resolve(__dirname, 'expected-commonjs.js')).toString();
-    assert(template === expected);
+    assert.equal(template, expected);
   });
 });
